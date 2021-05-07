bbf437634709a04f0a71b4f5b6bc6d78012ca168
RujutaB <60830306+RujutaB@users.noreply.github.com>
2021-05-07 19:42:11 +0530

    first commit

---
+++ domnetninja.js
@@ -0,0 +1,27 @@
+//document.getElementById
+
+//accessing and grabbing by element ID
+http://127.0.0.1:5500/domnetninja.htmldocument.getElementById('page-banner')
+//storing in a variable
+var banner = document.getElementById('page-banner')
+//calling the variable
+
+
+//document.getElementsByClassName
+
+//accessing and storing
+var titles = document.getElementsByClassName('title')
+//calling
+titles
+//calling individual elements cos there are 2 elements with same class name
+titles[0]
+
+document.getElementsByTagName('li')
+
+
+//QUERY SELECTOR
+
+
+
+//node and siblings
+const bookList = document.querySelector('#book-list');
